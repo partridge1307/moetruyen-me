e96e1fb618daeb8696b12ef7964e14416551c297
partridge <anhduc130703@gmail.com>
2023-11-22 01:30:03 +0700

    remove

--- prisma/migrations/20231121032223_add_team_cover/migration.sql
+++ prisma/migrations/20231121032223_add_team_cover/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `plainTextDescription` to the `Team` table without a default value. This is not possible if the table is not empty.
-  - Changed the type of `description` on the `Team` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
 -- AlterTable
 ALTER TABLE "Team" ADD COLUMN     "cover" TEXT,
 ADD COLUMN     "plainTextDescription" TEXT NOT NULL,
